69868d6f503a21feeb84b994f8e27688feaf6191
huangjinbao <huangjinbao@idea-group.cn>
2019-10-30 16:53:22 +0800

    first commit

--- src/components/global/tree-table.vue
+++ src/components/global/tree-table.vue
@@ -1,7 +1,45 @@
 <template>
-    <div class="tree-table">
-        $END$
-    </div>
+    <table
+        class="tree-table"
+        border="0"
+        cellSpacing="0"
+        cellPadding="10"
+    >
+        <thead>
+            <tr>
+                <th>项目编码</th>
+                <th>项目名称</th>
+                <th>项目指标</th>
+                <th>备注</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>1111</td>
+                <td>2222</td>
+                <td>3333</td>
+                <td>4444</td>
+            </tr>
+            <tr>
+                <td>1111</td>
+                <td>2222</td>
+                <td>3333</td>
+                <td>4444</td>
+            </tr>
+            <tr>
+                <td>1111</td>
+                <td>2222</td>
+                <td>3333</td>
+                <td>4444</td>
+            </tr>
+            <tr>
+                <td>1111</td>
+                <td>2222</td>
+                <td>3333</td>
+                <td>4444</td>
+            </tr>
+        </tbody>
+    </table>
 </template>
 
 <script>
@@ -11,5 +49,55 @@
 </script>
 
 <style lang="less" scoped>
+    @borderColor: #eee;
+    @hoverColor: #e9eaec;
+    @activeColor: #ffd9ab;
+    @stripeColor: #f8f9fb;
+    .tree-table{
+        width: 100%;
 
+        th, td{
+            font-size: 13px;
+            line-height: 23px;
+        }
+
+        th{
+            font-weight: normal;
+        }
+        thead{
+            tr{
+                background: @stripeColor;
+                &:hover{
+                    background: @hoverColor;
+                }
+            }
+        }
+        tbody{
+            tr{
+                &:nth-of-type(even){
+                    background: @stripeColor;
+                }
+                &:hover{
+                    background: @hoverColor;
+                }
+            }
+        }
+
+        &.border-table{
+            th, td{
+                border:1px solid @borderColor;
+                border-right: 0;
+                border-bottom: 0;
+
+                &:first-of-type{
+                    border-left:0;
+                }
+            }
+            tbody tr:last-of-type{
+                td{
+                    border-bottom: 1px solid @borderColor;
+                }
+            }
+        }
+    }
 </style>
