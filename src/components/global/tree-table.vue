3b7038dd44ce5b91c581cd8500ec30c44dfb172a
huangjinbao <huangjinbao@idea-group.cn>
2019-10-30 16:53:07 +0800

    first commit

---
+++ src/components/global/tree-table.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="tree-table">
+        $END$
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "tree-table"
+    }
+</script>
+
+<style lang="less" scoped>
+
+</style>
